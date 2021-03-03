d1c1c23c40208996908562b3744017bed1c6930c
Anand Joshy Kalappurakkal <anandjoshyk@outlook.com>
2021-03-03 20:04:54 +0100

    React Routing Completed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter,Route,Switch} from "react-router-dom"
+import Home from "./component/Home"
+import About from "./component/About"
+import SinglePost from "./component/SinglePost"
+import Post from "./component/Post"
+import Project from "./component/Projects"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about'/>
+        <Route component={SinglePost} path='/post/:slug'/>
+        <Route component={Post} path='/post'/>
+        <Route component={Project} path='/project'/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ portfolio/src/components/SinglePost.js
@@ -0,0 +1,5 @@
+
+export  default function SinglePost(){
+
+  return <h1> SinglePost Page </h1>
+}
