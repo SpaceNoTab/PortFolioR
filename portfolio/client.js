0c0110b5d39912cbf82aae067db9e463122ea7e1
Anand Joshy Kalappurakkal <anandjoshyk@outlook.com>
2021-03-04 21:42:31 +0100

    Post Complete -Sanity

--- portfolio/client.js
+++
@@ -1,6 +0,0 @@
-import sanityClient from  '@sanity/client'
-
-export default sanityClient({
-  projectId:"wizwe7ff",
-  dataset: "production"
-})
---
+++ portfolio/src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+  projectId: "wizwe7ff",
+  dataset: "production",
+});
