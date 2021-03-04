efe19ca0bd06bc7acdfc60835e8a0093f5946b0a
Anand Joshy Kalappurakkal <anandjoshyk@outlook.com>
2021-03-04 21:21:01 +0100

    Post.js- sanity connection completed

--- portfolio/src/components/Post.js
+++ portfolio/src/components/Post.js
@@ -1,6 +1,44 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client.js";
 
-export  default function Post(){
+export default function Post() {
+  const [postData, setPost] = useState(null);
 
-  return <h1> Post Page </h1>
+  useEffect(() => {
+    sanityClient.fetch(`*[_type=="post"]{
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+
+      }`);
+    then((data) => setPost(data)).catch(console.error);
+  }, []);
+  return (
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className=" container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive">Blog Post page</h1>
+        <h2 className="text-lg text-grat-600 flex justify-center mb-12">
+          Welcome to my page of blog
+        </h2>
+        <div className="grid md:grid-col-2 lg:grid-col-3 gap-8">
+          <article>
+            <Link>
+              <span>
+                <img />
+                <span>
+                  <h3></h3>
+                </span>
+              </span>
+            </Link>
+          </article>
+        </div>
+      </section>
+    </main>
+  );
 }
